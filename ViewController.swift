74a9b3ef5d49f343ee0edc3684ed61acba5ea010
Yasiru Jayatissa <110222524+yasirujayatissa@users.noreply.github.com>
2022-08-16 18:43:23 +0530

    First Project
    Details of the iOS team in cirlcles.asia. (made using scroll views and stacks mainly)

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTeamIos
+//
+//  Created by D.Yasiru Jayatissa on 2022-08-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
